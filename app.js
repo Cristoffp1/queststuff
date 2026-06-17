26ceeb792b614ab4d2257f8145d94b34ef4c678c
Cristoffp1 <Cristoffcaua123456@gmail.com>
2026-06-17 20:23:06 -0300

    Replace SVG figure with image for exercise display

--- app.js
+++ app.js
@@ -335,8 +335,8 @@ function openMissionScreen() {
           <div class="mission-screen-series">Série ${currentSet} de ${ex.sets}</div>
         </div>
       </div>
-            <div class="figure-container">
-        ${svgFigure(ex.pose, ex.mColor)}
+                  <div class="figure-container">
+        <img src="./${ex.pose}.png" class="exercise-image" alt="${ex.name}">
       </div>
       <div class="muscle-tags">
         ${ex.muscles.map(m => `<span class="muscle-tag" style="background:${ex.mColor}22;color:${ex.mColor}">${m}</span>`).join('')}
