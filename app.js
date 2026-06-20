bee1f6b9e69315b2451b248400376c83d28eb0ce
Cristoffp1 <Cristoffcaua123456@gmail.com>
2026-06-20 16:25:27 -0300

    Update app.js

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const SUPABASE_URL = 'https://flnzycipcdqtwcyujzqe.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZsbnp5Y2lwY2RxdHdjeXVqenFlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE2MjA5MzcsImV4cCI6MjA5NzE5NjkzN30.plqw_CtlaJmChDlnkTfHN9MUPVn0IK7Ty_WfujM3ICo'; 
 
 // Inicializa o cliente do Supabase diretamente no objeto window
-window.supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
+const supabaseClient = window.supabase.createClient(
+  SUPABASE_URL,
+  SUPABASE_KEY
+);
 
 // ===== EXERCISES DATA =====
 const EXERCISES = [
