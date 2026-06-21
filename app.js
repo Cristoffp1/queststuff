36ca58e6976548dad6d1274a091940860f355fe7
Cristoffp1 <Cristoffcaua123456@gmail.com>
2026-06-21 10:45:37 -0300

    Update Supabase URL in app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 // ===== SUPABASE CONFIG =====
-const SUPABASE_URL = 'https://flnzycipcdqtwcyujzqe.supabase.com';
+const SUPABASE_URL = 'https://flnzycipcdqtwcyujzqe.supabase.co';
 const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImZsbnp5Y2lwY2RxdHdjeXVqenFlIiwicm9sZSI6ImFub24iLCJpYXQiOjE3ODE2MjA5MzcsImV4cCI6MjA5NzE5NjkzN30.plqw_CtlaJmChDlnkTfHN9MUPVn0IK7Ty_WfujM3ICo'; 
 
 // Variável ÚNICA utilizada em todo o escopo do projeto
